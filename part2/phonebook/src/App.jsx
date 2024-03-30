890d1a9384536ae3546240d264d1932de627a0e1
pataphysick <qfwfq@pataphysick.org>
2024-03-30 15:05:09 -0700

    Exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -12,8 +12,13 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
-    const person = { name: newName }
-    setPersons(persons.concat(person))
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook.`)
+    }
+    else {
+      const person = { name: newName }
+      setPersons(persons.concat(person))
+    }
     setNewName('')
   }
 
