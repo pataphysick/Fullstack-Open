b510cee59581ddfc834ada3a0c59fd9a1b2258b4
pataphysick <qfwfq@pataphysick.org>
2024-04-09 20:59:19 -0700

    Part 2 Countries Exercise 2.18

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const CountryList = ({countries, filterString}) => {
+  const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(filterString.toLowerCase()))
+  if (filteredCountries.length > 10) {
+    return <p>Too many matches, specify another filter.</p>
+  }
+  else if (filteredCountries.length === 1) {
+    return <CountryInfo country={filteredCountries[0]} />
+  }
+  else {
+    return (
+      <>
+        {filteredCountries.map(country => <p key={country.cca2}>{country.name.common}</p>)}
+      </>
+    )
+  }
+}
+
+const CountryInfo = ({country}) => {
+  return (
+    <>
+      <h1>{country.name.common} {country.flag}</h1>
+      <p>Capital: {country.capital[0]}</p>
+      <p>Area: {country.area}</p>
+      <h3>Languages</h3>
+      <ul>
+    {Object.keys(country.languages).map(key => <li key={key}>{country.languages[key]}</li>)}
+      </ul>
+    </>
+  )
+}
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [filterString, setFilterString] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => setCountries(response.data))
+  }, [])
+
+  const handleFilterChange = (event) => {
+    setFilterString(event.target.value)
+  }
+
+  return (
+    <>
+      <p>Find countries:</p>
+      <input value={filterString} onChange={handleFilterChange} />
+      <CountryList countries={countries} filterString={filterString} />
+    </>
+  )
+}
+
+export default App
