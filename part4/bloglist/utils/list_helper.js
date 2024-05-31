15abec95ac50ceecefbcbb0c42c3d824c59776aa
pataphysick <qfwfq@pataphysick.org>
2024-05-30 19:59:55 -0700

    Exercise 4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -18,8 +20,36 @@ const favoriteBlog = (blogs) => {
     : blogs.reduce(compareBlogs)
 }
 
+const mostBlogs = (blogs) => {
+  const authorCount = lodash.countBy(blogs, (blog) => blog.author)
+
+  const maxBlogs = (old, value, key) => {
+    return old.blogs > value ? old : { author: key, blogs: value }
+  }
+
+  return blogs.length === 0
+    ? {}
+    : lodash.reduce(authorCount, maxBlogs, {})
+}
+
+const mostLikes = (blogs) => {
+  const authors = lodash.groupBy(blogs, (blog) => blog.author)
+
+  const countLikes = (old, value, key) => {
+    const likes = value.reduce((sum, blog) => sum = sum + blog.likes, 0)
+
+    return old.likes > likes ? old : { author: key, likes: likes }
+  }
+
+  return blogs.length === 0
+    ? {}
+    : lodash.reduce(authors, countLikes, {})
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
