6cac204946bf11250fa2d5970413be93b460e319
pataphysick <qfwfq@pataphysick.org>
2024-06-09 15:58:53 -0700

    Exercise 4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -10,9 +19,12 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response, next) => {
 
-  const users = await User.find({})
-  const user = users[0]
-  console.log(user)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'Token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({...request.body, user: user.id })
 
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -8,6 +8,9 @@ const errorHandler = (error, request, response, next) => {
   else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
     return response.status(400).json({ error: 'Expected "username" to be unique' })
   }
+  else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'Token invalid' })
+  }
   next(error)
 }
 
