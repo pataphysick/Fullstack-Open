bf562252c99c01ec58ad953ec7ed22bba7893ecc
pataphysick <qfwfq@pataphysick.org>
2024-07-20 15:12:39 -0700

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setView(!view)
+  }
+
+  const expanded = () => {
+    return (
+      <>
+        <p>URL: {blog.url}</p>
+        <p>Likes: {blog.likes} <button>Like</button></p>
+      </>
+    )
+  }
+
+  return(
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>{view ? 'hide' : 'view'}</button>
+      </div>
+      {view ? expanded() : null}
+    </div>
+  )}
 
 export default Blog
